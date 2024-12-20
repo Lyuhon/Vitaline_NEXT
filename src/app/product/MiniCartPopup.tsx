6349405f7232527c7abb6e838a090f145a33aa7e
Lyuhon <lyuhon.uz@gmail.com>
2024-12-20 12:52:24 +0500

    Минорная Сборка v 0.6.2 (Стили миникарты)

--- src/app/product/MiniCartPopup.tsx
+++ src/app/product/MiniCartPopup.tsx
@@ -15,14 +15,22 @@ const MiniCartPopup: React.FC<MiniCartPopupProps> = ({ productImage, productName
     return (
         <div className="actb_popup_overlay" onClick={onClose}>
             <div className="actb_popup_content" onClick={(e) => e.stopPropagation()}>
-                <button className="actb_close_button" onClick={onClose} aria-label="Закрыть">&times;</button>
-                <img src={productImage} alt={productName} className="actb_popup_product_image" />
-                <h4 className="actb_popup_title">Товар добавлен в корзину</h4>
-                <p className="actb_popup_product_name"><strong>{productName}</strong></p>
-                <p className="actb_popup_quantity">Количество: {quantity}</p>
-                <Link href="/cart" className="actb_popup_button">
-                    Перейти к корзине
-                </Link>
+                {/* <button className="actb_close_button" onClick={onClose} aria-label="Закрыть">&times;</button> */}
+                <span className="actb_close_button" onClick={onClose} aria-label="Закрыть">×</span>
+
+                <div className="added_to_cart_pop">
+                    <img src={productImage} alt={productName} className="actb_popup_product_image" />
+
+                    <div className="pop_prod_inf">
+                        <h4 className="actb_popup_title">Товар добавлен в корзину</h4>
+                        <p className="actb_popup_product_name">{productName}</p>
+                        <p className="actb_popup_quantity"><span className='actb_popup_quantity_str'>Количество: </span>{quantity}</p>
+                        <Link href="/cart" className="actb_popup_button">
+                            Просмотреть корзину
+                        </Link>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
