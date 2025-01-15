001525949a4c96006f2055aa429e5f444e46cea9
Lyuhon <lyuhon.uz@gmail.com>
2025-01-16 02:28:07 +0500

    Предрелизная сборка v 0.9.5 (Добавил float cart)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,73 @@
+// // // src/app/layout.tsx
+// import './globals.css';
+// import Header from '@/components/Header';
+// import Footer from '@/components/Footer';
+// import PageTransition from '@/components/PageTransition';
+// // import AnimatedWrapper from '@/components/animation/AnimatedWrapper';
+
+// import { Metadata } from 'next';
+// // import Script from 'next/script';
+
+// import { Suspense } from "react";
+// import { Metrika } from "@/components/Metrika";
+
+
+// export const metadata: Metadata = {
+//   title: 'Интернет магазин витаминов - Vitaline',
+// };
+
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang="ru">
+//       <head>
+//         <meta charSet="UTF-8" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//         <link
+//           rel="stylesheet"
+//           href="https://unpkg.com/swiper/swiper-bundle.min.css"
+//         />
+//         <link
+//           rel="icon"
+//           type="image/png"
+//           sizes="32x32"
+//           href="https://www.vitaline.uz/wp-content/themes/vl/assets/img/favicons/favicon-32x32.png"
+//         />
+//       </head>
+//       <body>
+//         <Header />
+//         {/* <AnimatedWrapper> */}
+//         {/* Был рабочий */}
+//         {/* <PageTransition> */}
+//         {/* <div id="page-transition-wrapper" className="content-wrapper page-transition"> */}
+
+//         <div className="content-wrapper">
+//           {children}
+//         </div>
+
+//         {/* </PageTransition> */}
+//         {/* </AnimatedWrapper> */}
+//         <Footer />
+//         <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+//         <Suspense>
+//           <Metrika />
+//         </Suspense>
+//       </body>
+//     </html >
+//   );
+// }
+
+
+
+
+
 // // src/app/layout.tsx
 import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import PageTransition from '@/components/PageTransition';
+// import PageTransition from '@/components/PageTransition';
 // import AnimatedWrapper from '@/components/animation/AnimatedWrapper';
+
+import Script from 'next/script';
 
 import { Metadata } from 'next';
 // import Script from 'next/script';
@@ -47,7 +111,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* </PageTransition> */}
         {/* </AnimatedWrapper> */}
         <Footer />
-        <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+        <Script
+          // src="https://unpkg.com/swiper/swiper-bundle.min.js"
+          src="/swiper-bundle.min.js"
+          strategy="beforeInteractive"
+          // Добавляем кеширование
+          crossOrigin="anonymous"
+        />
         <Suspense>
           <Metrika />
         </Suspense>
